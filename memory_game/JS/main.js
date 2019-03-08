7cf4911679c161bd973add976112909ffcff0991
shadiboi <saoutabachi@gmail.com>
2019-03-08 16:04:25 -0700

    added game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,20 +1,26 @@
-console.log("Up and running!");
-
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cards[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log ('User flipped ' + cardOne);
-console.log ('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+var flipCard = function(cardId) {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
 	alert("You have found a match!");
 else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again!");
+	alert("Sorry, try again!");
 	}
+	{console.log("User flipped " + cards[cardId])};
+	cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
 
 
 
